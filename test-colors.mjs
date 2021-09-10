43e36faa22f759bb03bd0f2cd7aa18fdd4f9d49d
Matthieu Napoli <matthieu@mnapoli.fr>
2021-09-10 16:55:08 +0200

    Improve script to test colors

--- test-colors.mjs
+++ test-colors.mjs
@@ -1,5 +1,6 @@
 import styles from 'ansi-styles';
 
-console.log(`${styles.color.ansi16m(...styles.hexToRgb('#fd5750'))}✔ 16m colors${styles.color.close}`)
-console.log(`${styles.color.ansi256(styles.hexToAnsi256('#fd5750'))}✔ ANSI 256 colors${styles.color.close}`)
-console.log(`${styles.color.ansi(styles.hexToAnsi('#fd5750'))}✔ ANSI 16 colors${styles.color.close}`)
+console.log(`${styles.red.open}✔ base ANSI red${styles.red.close}`)
+console.log(`${styles.color.ansi(styles.hexToAnsi('#fd5750'))}✔ Serverless red converted to ANSI 16 colors (code: ${styles.hexToAnsi('#fd5750')})${styles.color.close}`)
+console.log(`${styles.color.ansi256(styles.hexToAnsi256('#fd5750'))}✔ Serverless red converted to ANSI 256 colors (code: ${styles.hexToAnsi256('#fd5750')} = 'LightCoral' - #ff8787)${styles.color.close}`)
+console.log(`${styles.color.ansi16m(...styles.hexToRgb('#fd5750'))}✔ Serverless red in 16m colors (#fd5750)${styles.color.close}`)
