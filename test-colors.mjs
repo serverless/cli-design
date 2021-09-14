6a9dad6e2e533f091c76caa3e4f31926c9162ab5
Matthieu Napoli <matthieu@mnapoli.fr>
2021-09-14 16:50:36 +0200

    Test bright red

--- test-colors.mjs
+++ test-colors.mjs
@@ -1,6 +1,7 @@
 import styles from 'ansi-styles';
 
 console.log(`${styles.red.open}✔ base ANSI red${styles.red.close}`)
+console.log(`${styles.redBright.open}✔ base ANSI bright red${styles.redBright.close}`)
 console.log(`${styles.color.ansi(styles.hexToAnsi('#fd5750'))}✔ Serverless red converted to ANSI 16 colors (code: ${styles.hexToAnsi('#fd5750')})${styles.color.close}`)
 console.log(`${styles.color.ansi256(styles.hexToAnsi256('#fd5750'))}✔ Serverless red converted to ANSI 256 colors (code: ${styles.hexToAnsi256('#fd5750')} = 'LightCoral' - #ff8787)${styles.color.close}`)
 console.log(`${styles.color.ansi16m(...styles.hexToRgb('#fd5750'))}✔ Serverless red in 16m colors (#fd5750)${styles.color.close}`)
